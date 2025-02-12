10853a1004c706d5be52957cdc3bfb514c06cc3b
Mukul Malhotra <malhotramukul1122@gmail.com>
2025-02-12 15:42:05 +0000

    Practising type annotation in variables and functions

--- basics.js
+++
@@ -1,4 +0,0 @@
-var pi = 3.14159;
-console.log("Hello");
-console.log("Bye");
-console.log(Math.round(6.3456484564865468));
--- basics.ts
+++
@@ -1,6 +0,0 @@
-let pi: number = 3.14159;
-console.log("Hello");
-console.log("Bye");
-console.log(Math.round(6.3456484564865468));
-
-// let myVar: type = value
